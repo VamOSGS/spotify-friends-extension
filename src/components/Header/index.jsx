192e0acfaf5fd1e1db8619604303ea1fc0a38be0
vamosgs <gegham.samvelyan@hotmail.com>
2022-08-29 21:29:14 +0400

    Header update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './header.css';
 
-export default function Header({ title = ' Spotify With Friends' }) {
+const imageSize = {
+  lg: '72px',
+  sm: '32px'
+}
+
+export default function Header({ title = ' Spotify With Friends', size = 'lg' }) {
   return (
     <header className='app-header'>
-      <img src='/assets/icons/icon-72x72.png' alt='Spotify logo' />
+      <img src='/assets/icons/icon-72x72.png' alt='Spotify logo' width={imageSize[size]} />
       <span className='header-title'>{title}</span>
     </header>
   );
--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -1,9 +1,10 @@
+import Header from 'components/Header';
 import React from 'react';
 
 function Popup() {
   return (
     <div className='popup'>
-      
+      <Header size='sm' />
     </div>
   );
 }
